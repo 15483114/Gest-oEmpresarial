8d7d6e91f10ad8f95e03a4b8f6a9db9bf2273925
matheus <matheus2060@gmail.com>
2021-07-17 18:14:11 -0300

    updated list

--- app.py
+++ app.py
@@ -23,29 +23,20 @@ def cat1_survey_page():
     radio_field = ''
     str_field = ''
 
-
     if request.method == 'POST':
-
-        # check that we have all the required fields to append to file
+        # verefica se os campos de pesquisa estão presentes
         str_field = request.form['str_field']
-        # remove special characters from input for security
-        str_field = re.sub(r"[^a-zA-Z0-9]","",str_field)
-
         first_name = request.form['first_name']
-        last_name = request.form['last_name']
+        last_name = request.form['last_name'] 
         email = request.form['email']
-
-        # optional fields
-        if 'radio_field' in request.form:
-            radio_field = request.form['radio_field']
+        if 'gender' in request.form:
+            gender = request.form['gender']
         else:
-            radio_field = 'NA'
-
-
-        # check that essential fields have been filled
+            gender = 'NA'
+        
+        # verifica se os campos essênciais estão preenchidos
         message = ''
         missing_required_answers_list = []
-            
         if str_field == '':
             missing_required_answers_list.append('Tells us more')
         if first_name == '':
@@ -55,27 +46,13 @@ def cat1_survey_page():
         if email == '':
             missing_required_answers_list.append('Email')
 
-
         if len(missing_required_answers_list) > 0:
-            # return back a string with missing fields
-            message = '<div class="w3-row-padding w3-padding-16 w3-center"><H3>You missed the following question(s):</H3><font style="color:red;">'
+            # retorna uma string com os campos vazios
+            message = '<div class="w3-row-padding w3-padding-16 w3-center"><H3>Você não preencheu os seguinte(s) campo(s):</H3><font style="color:red;">'
             for ms in missing_required_answers_list:
                 message += '<BR>' + str(ms)
             message += '</font></div>'
-        
-            return render_template('cat1.html',
-                                    message = Markup(message),
-                                    first_name = first_name,
-                                    last_name = last_name,
-                                    email = email,
-                                    radio_field = radio_field,
-                                    str_field = str_field)
-        else:
-            # append survey answers to data array
-            survey_data.extend([first_name, last_name, email, str_field, radio_field])
-            
-            return render_template('cat2.html')
-
+    
     return render_template('cat1.html',
                             message = Markup(message),
                             first_name = first_name,
@@ -83,11 +60,6 @@ def cat1_survey_page():
                             email = email,
                             radio_field = radio_field,
                             str_field = str_field)
-
-@app.route("/", methods=['POST', 'GET'])
-def cat2_survey_page():
-
-    return ""
     
 
 
