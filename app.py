4442ac18cdd19d29721a1baf3af627310114f930
matheus <matheus2060@gmail.com>
2021-07-20 18:56:49 -0300

    observações para cat 2 e 3

--- app.py
+++ app.py
@@ -36,30 +36,25 @@ def __salvar_dados_csv():
     with open(BASE_DIR + '/surveys/survey_samp_1.csv','a+') as myfile: # use a+ to append and create file if it doesn't exist
             myfile.write(str_dados)
 
+    id_data.clear()
+    categoria_1_data.clear()
+    categoria_2_data.clear()
+    categoria_3_data.clear()
+
 
 # survey page
 @app.route("/", methods=['POST', 'GET'])
 @app.route("/cat1", methods=['POST', 'GET'])
 def cat1():
-    message = ''
-    if len(id_data) == 0 :
-        nome_examinador = ''
-        nome_avaliado = ''
-        email = ''
-        genero = ''
-        pergunta_1_1 = ''
-        aval_1_1 = ''
-        pergunta_1_2 = ''
-        aval_1_2 = ''
-    else :
-        nome_examinador = id_data[1]
-        nome_avaliado = id_data[2]
-        email = id_data[3]
-        genero = id_data[4]
-        pergunta_1_1 = categoria_1_data[0]
-        aval_1_1 = categoria_1_data[1]
-        pergunta_1_2 = categoria_1_data[2]
-        aval_1_2 = categoria_1_data[3]
+    message = ''  
+    nome_examinador = ''
+    nome_avaliado = ''
+    email = ''
+    genero = ''
+    pergunta_1_1 = ''
+    aval_1_1 = ''
+    pergunta_1_2 = ''
+    aval_1_2 = ''
 
 
     if request.method == 'POST':
@@ -86,12 +81,8 @@ def cat1():
             if genero == '':
                 missing_required_answers_list.append('Genêro do examinador')
 
-            if pergunta_1_1 == '':
-                missing_required_answers_list.append('Pergunta 1.1')
             if aval_1_1 == '':
                 missing_required_answers_list.append('Avaliação para pergunta 1.1')
-            if pergunta_1_2 == '':
-                missing_required_answers_list.append('Pergunta 1.2')
             if aval_1_2 == '':
                 missing_required_answers_list.append('Avaliação para pergunta 1.2')
             
@@ -118,10 +109,10 @@ def cat1():
                 id_data.append(email)
                 id_data.append(genero)
 
-                categoria_1_data.append(pergunta_1_1)
                 categoria_1_data.append(aval_1_1)
-                categoria_1_data.append(pergunta_1_2)
                 categoria_1_data.append(aval_1_2)
+                categoria_1_data.append(pergunta_1_1)
+                categoria_1_data.append(pergunta_1_2)
 
                 return redirect(url_for('cat2'))
     
@@ -145,6 +136,11 @@ def cat2():
     aval_2_3 = ''
     aval_2_4 = ''
     aval_2_5 = ''
+    pergunta_2_1 = ''
+    pergunta_2_2 = ''
+    pergunta_2_3 = ''
+    pergunta_2_4 = ''
+    pergunta_2_5 = ''
     
     if request.method == 'POST':
         if request.form['action'] == 'Para categoria 1':
@@ -157,21 +153,26 @@ def cat2():
             aval_2_3 = request.form['aval_2_3']
             aval_2_4 = request.form['aval_2_4']
             aval_2_5 = request.form['aval_2_5']
+            pergunta_2_1 = request.form['pergunta_2_1'].strip()
+            pergunta_2_2 = request.form['pergunta_2_2'].strip()
+            pergunta_2_3 = request.form['pergunta_2_3'].strip()
+            pergunta_2_4 = request.form['pergunta_2_4'].strip()
+            pergunta_2_5 = request.form['pergunta_2_5'].strip()
 
             # verifica se os campos essênciais estão preenchidos
             message = ''
             missing_required_answers_list = []
     
             if aval_2_1 == '':
-                missing_required_answers_list.append('Pergunta 2.1')
+                missing_required_answers_list.append('Avaliação para pergunta 2.1')
             if aval_2_2 == '':
-                    missing_required_answers_list.append('Pergunta 2.2')
+                    missing_required_answers_list.append('Avaliação para pergunta 2.2')
             if aval_2_3 == '':
-                    missing_required_answers_list.append('Pergunta 2.3')
+                    missing_required_answers_list.append('Avaliação para pergunta 2.3')
             if aval_2_4 == '':
-                    missing_required_answers_list.append('Pergunta 2.4')
+                    missing_required_answers_list.append('Avaliação para pergunta 2.4')
             if aval_2_5 == '':
-                    missing_required_answers_list.append('Pergunta 2.5')
+                    missing_required_answers_list.append('Avaliação para pergunta 2.5')
 
             if len(missing_required_answers_list) > 0:
                 # retorna uma string com os campos vazios
@@ -182,15 +183,19 @@ def cat2():
             else:
                 message = ''
                 # Cria marcação de tempo para entrada
-               
+
+                categoria_2_data.clear()
             
                 categoria_2_data.append(aval_2_1)
                 categoria_2_data.append(aval_2_2)
                 categoria_2_data.append(aval_2_3)
                 categoria_2_data.append(aval_2_4)
                 categoria_2_data.append(aval_2_5)
-
-                __salvar_dados_csv()
+                categoria_2_data.append(pergunta_2_1)
+                categoria_2_data.append(pergunta_2_2)
+                categoria_2_data.append(pergunta_2_3)
+                categoria_2_data.append(pergunta_2_4)
+                categoria_2_data.append(pergunta_2_5)
 
                 return redirect(url_for('cat3'))
 
@@ -201,7 +206,12 @@ def cat2():
                             aval_2_2 = aval_2_2,
                             aval_2_3 = aval_2_3,
                             aval_2_4 = aval_2_4,
-                            aval_2_5 = aval_2_5)
+                            aval_2_5 = aval_2_5,
+                            pergunta_2_1 = pergunta_2_1,
+                            pergunta_2_2 = pergunta_2_2,
+                            pergunta_2_3 = pergunta_2_3,
+                            pergunta_2_4 = pergunta_2_4,
+                            pergunta_2_5 = pergunta_2_5)
 
 @app.route("/cat3", methods=['POST', 'GET'])
 def cat3():
@@ -213,6 +223,14 @@ def cat3():
     aval_3_6 = ''
     aval_3_7 = ''
     aval_3_8 = ''
+    pergunta_3_1 = ''
+    pergunta_3_2 = ''
+    pergunta_3_3 = ''
+    pergunta_3_4 = ''
+    pergunta_3_5 = ''
+    pergunta_3_6 = ''
+    pergunta_3_7 = ''
+    pergunta_3_8 = ''
     message = ''
     
 
@@ -231,21 +249,29 @@ def cat3():
             aval_3_6 = request.form['aval_3_6']
             aval_3_7 = request.form['aval_3_7']
             aval_3_8 = request.form['aval_3_8']
+            pergunta_3_1 = request.form['pergunta_3_1'].strip()
+            pergunta_3_2 = request.form['pergunta_3_2'].strip()
+            pergunta_3_3 = request.form['pergunta_3_3'].strip()
+            pergunta_3_4 = request.form['pergunta_3_4'].strip()
+            pergunta_3_5 = request.form['pergunta_3_5'].strip()
+            pergunta_3_6 = request.form['pergunta_3_6'].strip()
+            pergunta_3_7 = request.form['pergunta_3_7'].strip()
+            pergunta_3_8 = request.form['pergunta_3_8'].strip()
 
             # verifica se os campos essênciais estão preenchidos
             message = ''
             missing_required_answers_list = []
     
             if aval_3_1 == '':
-                missing_required_answers_list.append('Pergunta 3.1')
+                missing_required_answers_list.append('Avaliação para pergunta 3.1')
             if aval_3_2 == '':
-                    missing_required_answers_list.append('Pergunta 3.2')
+                    missing_required_answers_list.append('Avaliação para pergunta 3.2')
             if aval_3_3 == '':
-                    missing_required_answers_list.append('Pergunta 3.3')
+                    missing_required_answers_list.append('Avaliação para pergunta 3.3')
             if aval_3_4 == '':
-                    missing_required_answers_list.append('Pergunta 3.4')
+                    missing_required_answers_list.append('Avaliação para pergunta 3.4')
             if aval_3_5 == '':
-                    missing_required_answers_list.append('Pergunta 3.5')
+                    missing_required_answers_list.append('Avaliação para pergunta 3.5')
 
             if len(missing_required_answers_list) > 0:
                 # retorna uma string com os campos vazios
@@ -255,9 +281,9 @@ def cat3():
                 message += '</font></div>'
             else:
                 message = ''
-                # Cria marcação de tempo para entrada
-                entry_time = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-                categoria_3_data.append(entry_time)
+
+                categoria_3_data.clear()
+
                 categoria_3_data.append(aval_3_1)
                 categoria_3_data.append(aval_3_2)
                 categoria_3_data.append(aval_3_3)
@@ -266,7 +292,17 @@ def cat3():
                 categoria_3_data.append(aval_3_6)
                 categoria_3_data.append(aval_3_7)
                 categoria_3_data.append(aval_3_8)
+                categoria_3_data.append(pergunta_3_1)
+                categoria_3_data.append(pergunta_3_2)
+                categoria_3_data.append(pergunta_3_3)
+                categoria_3_data.append(pergunta_3_4)
+                categoria_3_data.append(pergunta_3_5)
+                categoria_3_data.append(pergunta_3_6)
+                categoria_3_data.append(pergunta_3_7)
+                categoria_3_data.append(pergunta_3_8)
+
                 __salvar_dados_csv()
+
                 return redirect(url_for('results'))
 
 
@@ -280,7 +316,15 @@ def cat3():
                     aval_3_5 = aval_3_5,
                     aval_3_6 = aval_3_6,
                     aval_3_7 = aval_3_7,
-                    aval_3_8 = aval_3_8)
+                    aval_3_8 = aval_3_8,
+                    pergunta_3_1 = pergunta_3_1,
+                    pergunta_3_2 = pergunta_3_2,
+                    pergunta_3_3 = pergunta_3_3,
+                    pergunta_3_4 = pergunta_3_4,
+                    pergunta_3_5 = pergunta_3_5,
+                    pergunta_3_6 = pergunta_3_6,
+                    pergunta_3_7 = pergunta_3_7,
+                    pergunta_3_8 = pergunta_3_8)
 
 @app.route("/results", methods=['POST', 'GET'])
 def results():
