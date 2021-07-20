e7553a49c08c6cd12b7a0266ffde4ba517847ea0
matheus <matheus2060@gmail.com>
2021-07-19 22:52:42 -0300

    routing funcionando

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, app, render_template, request, Markup
+from flask import Flask, session, app, render_template, request, Markup, url_for, redirect
 import sys, io, re
 import os, base64
 from io import StringIO
@@ -32,74 +32,93 @@ def __salvar_dados_csv():
 
 # survey page
 @app.route("/", methods=['POST', 'GET'])
-def cat1_survey_page():
+@app.route("/cat1", methods=['POST', 'GET'])
+def cat1():
     message = ''
-    nome_examinador = ''
-    nome_avaliado = ''
-    email = ''
-    genero = ''
-    pergunta_1_1 = ''
-    aval_1_1 = ''
-    pergunta_1_2 = ''
-    aval_1_2 = ''
+    if len(id_data) == 0 :
+        nome_examinador = ''
+        nome_avaliado = ''
+        email = ''
+        genero = ''
+        pergunta_1_1 = ''
+        aval_1_1 = ''
+        pergunta_1_2 = ''
+        aval_1_2 = ''
+    else :
+        nome_examinador = id_data[1]
+        nome_avaliado = id_data[2]
+        email = id_data[3]
+        genero = id_data[4]
+        pergunta_1_1 = categoria_1_data[0]
+        aval_1_1 = categoria_1_data[1]
+        pergunta_1_2 = categoria_1_data[2]
+        aval_1_2 = categoria_1_data[3]
+
 
     if request.method == 'POST':
-        # verefica se os campos de pesquisa estão presentes
-        nome_examinador = request.form['nome_examinador'].strip()
-        nome_avaliado = request.form['nome_avaliado'].strip()
-        email = request.form['email'].strip()
-        genero = request.form['genero'].strip()
-        pergunta_1_1 = request.form['pergunta_1_1'].strip()
-        aval_1_1 = request.form['aval_1_1']
-        pergunta_1_2 = request.form['pergunta_1_2'].strip()
-        aval_1_2 = request.form['aval_1_2']
-        
-        # verifica se os campos essênciais estão preenchidos
-        message = ''
-        missing_required_answers_list = []
-        if nome_examinador == '':
-            missing_required_answers_list.append('Nome do examinador')
-        if nome_avaliado == '':
-            missing_required_answers_list.append('Nome do avaliado')
-        if email == '':
-            missing_required_answers_list.append('Email do examinador')
-        if genero == '':
-            missing_required_answers_list.append('Genêro do examinador')
-
-        if pergunta_1_1 == '':
-            missing_required_answers_list.append('Pergunta 1.1')
-        if aval_1_1 == '':
-            missing_required_answers_list.append('Avaliação para pergunta 1.1')
-        if pergunta_1_2 == '':
-            missing_required_answers_list.append('Pergunta 1.2')
-        if aval_1_2 == '':
-            missing_required_answers_list.append('Avaliação para pergunta 1.2')
-        
-
-        if len(missing_required_answers_list) > 0:
-            # retorna uma string com os campos vazios
-            message = '<div class="w3-row-padding w3-padding-16 w3-center"><H3>Você não preencheu os seguinte(s) campo(s):</H3><font style="color:red;">'
-            for ms in missing_required_answers_list:
-                message += '<BR>' + str(ms)
-            message += '</font></div>'
-        else:
-            message = ''
-            # Cria marcação de tempo para entrada
-            entry_time = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        if request.form['action'] == 'Para categoria 2':
+            # verefica se os campos de pesquisa estão presentes
+            nome_examinador = request.form['nome_examinador'].strip()
+            nome_avaliado = request.form['nome_avaliado'].strip()
+            email = request.form['email'].strip()
+            genero = request.form['genero'].strip()
+            pergunta_1_1 = request.form['pergunta_1_1'].strip()
+            aval_1_1 = request.form['aval_1_1']
+            pergunta_1_2 = request.form['pergunta_1_2'].strip()
+            aval_1_2 = request.form['aval_1_2']
             
-            id_data.append(entry_time)
-            id_data.append(nome_examinador)
-            id_data.append(nome_avaliado)
-            id_data.append(email)
-            id_data.append(genero)
+            # verifica se os campos essênciais estão preenchidos
+            message = ''
+            missing_required_answers_list = []
+            if nome_examinador == '':
+                missing_required_answers_list.append('Nome do examinador')
+            if nome_avaliado == '':
+                missing_required_answers_list.append('Nome do avaliado')
+            if email == '':
+                missing_required_answers_list.append('Email do examinador')
+            if genero == '':
+                missing_required_answers_list.append('Genêro do examinador')
 
-            categoria_1_data.append(pergunta_1_1)
-            categoria_1_data.append(aval_1_1)
-            categoria_1_data.append(pergunta_1_2)
-            categoria_1_data.append(aval_1_2)
+            if pergunta_1_1 == '':
+                missing_required_answers_list.append('Pergunta 1.1')
+            if aval_1_1 == '':
+                missing_required_answers_list.append('Avaliação para pergunta 1.1')
+            if pergunta_1_2 == '':
+                missing_required_answers_list.append('Pergunta 1.2')
+            if aval_1_2 == '':
+                missing_required_answers_list.append('Avaliação para pergunta 1.2')
+            
 
-            __salvar_dados_csv()
-            return render_template('cat2.html')
+            if len(missing_required_answers_list) > 0:
+                # retorna uma string com os campos vazios
+                message = '<div class="w3-row-padding w3-padding-16 w3-center"><H3>Você não preencheu os seguinte(s) campo(s):</H3><font style="color:red;">'
+                for ms in missing_required_answers_list:
+                    message += '<BR>' + str(ms)
+                message += '</font></div>'
+            else:
+                message = ''
+
+                # Limpa a lista antes de adicionar os novos valores
+                id_data.clear()
+                categoria_1_data.clear()
+
+                # Cria marcação de tempo para entrada
+                entry_time = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+                
+                id_data.append(entry_time)
+                id_data.append(nome_examinador)
+                id_data.append(nome_avaliado)
+                id_data.append(email)
+                id_data.append(genero)
+
+                categoria_1_data.append(pergunta_1_1)
+                categoria_1_data.append(aval_1_1)
+                categoria_1_data.append(pergunta_1_2)
+                categoria_1_data.append(aval_1_2)
+
+                __salvar_dados_csv()
+
+                return redirect(url_for('cat2'))
     
     return render_template('cat1.html',
                             message = Markup(message),
@@ -112,10 +131,29 @@ def cat1_survey_page():
                             pergunta_1_2 = pergunta_1_2,
                             aval_1_2 = aval_1_2)
 
-@app.route("/", methods=['POST', 'GET'])
-def cat2_survey_page():
 
-    return ""
+@app.route("/cat2", methods=['POST', 'GET'])
+def cat2():
+    
+    if request.method == 'POST':
+        if request.form['action'] == 'Para categoria 1':
+
+            return redirect(url_for('cat1'))
+        elif request.form['action'] == 'Para categoria 3':
+
+            return redirect(url_for('cat3'))
+
+    # show the form, it wasn't submitted
+    return render_template('cat2.html')
+
+@app.route("/cat3", methods=['POST', 'GET'])
+def cat3():
+    
+    if request.method == 'POST':
+        print()
+
+    # show the form, it wasn't submitted
+    return render_template('cat3.html')
     
 
 
